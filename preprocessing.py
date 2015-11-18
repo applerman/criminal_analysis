fbb37a4f8caa2d7ac06eee3892ee36f0980c6025
Jenny Chen <jenny350026@hotmail.com>
2015-11-17 16:46:55 -0800

    put attributes into dictionareis

--- preprocessing.py
+++ preprocessing.py
@@ -2,10 +2,35 @@ import csv
 import numpy
 
 i = 0
+
 with open('train.csv', 'rb') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+    raw_data = dict()
+    attr_names = spamreader.next() #['Date, 'Category', ...]
+    for attr in attr_names:
+        raw_data[attr] = [] # raw_data['Date'] = []
+                            # raw_data['Category'] = []
+
     for row in spamreader:
-        if i < 2:
-            for field in row:
-                print field
-        i = i + 1
+        for (i, attr) in enumerate(row): #"2011-01-12 17:05:00", "
+            raw_data[attr_names[i]].append(attr)
+            
+
+    # Dates -> split into year, month, date, hour (24hr), minute, second (may be all 0)
+
+    # Category -> label them from 0-38
+
+    # Descript -> ???
+
+    # DayOfWeek -> Convert to 7 binary features (Monday, …, Sunday)
+
+    # PdDistrict -> Convert to # binary features
+
+    # Resolution -> ???
+
+    # Address -> ???
+
+    # X -> will use raw data, as well as features incorporated from SF OpenData
+
+    # Y -> same as above
+            
