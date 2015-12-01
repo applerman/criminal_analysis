fd70de6ce147b7dd9f94352f13afd3a6a43afa55
eugenegx <eugenegx@gmail.com>
2015-12-01 00:04:32 -0600

    Fast distance feature from SF Open

--- generateDistanceWirelessFood.py
+++ generateDistanceWirelessFood.py
@@ -1,4 +1,4 @@
-from geopy.distance import vincenty
+from math import sqrt, pow
 import csv
 
 data_wireless = []
@@ -59,6 +59,10 @@ def generateFeature(inputFilename, featureFilename):
                 raw_data[attr] = []  # raw_data['Date'] = []
                                      # raw_data['Category'] = []
 
+            count = 0
+            dataToWrite = []
+            maxInvDistanceWireless = 0
+            maxInvDistanceFood = 0
             # Write to CSV
             if featureFilename:
                 with open(featureFilename, 'wb') as f:
@@ -68,19 +72,33 @@ def generateFeature(inputFilename, featureFilename):
                     for row in spamreader:
                         for (i, attr) in enumerate(row): #"2011-01-12 17:05:00", "
                             if attr_names[i] == 'Y':
-                                lat = attr
+                                lat = float(attr)
                             if attr_names[i] == 'X':
-                                lon = attr
+                                lon = float(attr)
+
+                        count += 1
+                        if count % 1000 == 0:
+                            print count
 
                         d = [lat, lon]
-                        distanceWireless = min(map(lambda x: vincenty(d, x).miles, data_wireless))
-                        distanceFood = min(map(lambda x: vincenty(d, x).miles, data_food))
-                        writerFeature.writerow([distanceWireless,distanceFood,1/distanceWireless,1/distanceFood])
+                        distanceWireless = min(map(lambda x: sqrt(pow(d[0]-x[0], 2)+pow(d[1]-x[1], 2)), data_wireless))
+                        distanceFood = min(map(lambda x: sqrt(pow(d[0]-x[0], 2)+pow(d[1]-x[1], 2)), data_food))
+                        maxInvDistanceWireless = max(maxInvDistanceWireless, 1/distanceWireless)
+                        maxInvDistanceFood = max(maxInvDistanceFood, 1/distanceFood)
+                        dataToWrite.append([distanceWireless, distanceFood])
+
+                    for row in dataToWrite:
+                        distanceWireless = row[0]
+                        distanceFood = row[1]
+                        writerFeature.writerow([distanceWireless, distanceFood, 1/distanceWireless/maxInvDistanceWireless, 1/distanceFood/maxInvDistanceFood])
 
 
 def main():
+    print "prepareData"
     prepareData('Recreation/Wireless_Service_Facility_Permits.csv','Recreation/Mobile_Food_Facility_Permit.csv')
+    print "generateFeature train"
     generateFeature('train.csv', 'trainDistanceWirelessFood.csv')
+    print "generateFeature test"
     generateFeature('test.csv', 'testFeatureWirelessFood.csv')
 
 if __name__ == "__main__":
