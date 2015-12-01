0516bb0b4bc40d44e2001ca721f16249ede8fa4c
chunheng <chunheng@utexas.edu>
2015-12-01 10:20:12 -0600

    normalized

--- linearTrain.py
+++ linearTrain.py
@@ -5,15 +5,21 @@ from numpy import insert
 
 def main():
 
-  trainFeature = genfromtxt('trainFeature.csv', delimiter=',')[0::5]
-  trainLabel = genfromtxt('trainLabel.csv', delimiter='\n')[0::5]
-  testFeature = genfromtxt('testFeature.csv', delimiter=',')   
+  #trainFeature = genfromtxt('trainFeature.csv', delimiter=',')[0::5]
+  #trainLabel = genfromtxt('trainLabel.csv', delimiter='\n')[0::5]
+  #testFeature = genfromtxt('testFeature.csv', delimiter=',')   
+
+  trainFeature = genfromtxt('mytrain.csv', delimiter=',')[0::5]
+  trainLabel = genfromtxt('mylabel.csv', delimiter='\n')[0::5]
+  testFeature = genfromtxt('mytest.csv', delimiter=',')   
 
   lin_clf = svm.LinearSVC()
   lin_clf.fit(trainFeature, trainLabel)
   dec = lin_clf.decision_function(testFeature)
   
   header = "Id,ARSON,ASSAULT,BAD CHECKS,BRIBERY,BURGLARY,DISORDERLY CONDUCT,DRIVING UNDER THE INFLUENCE,DRUG/NARCOTIC,DRUNKENNESS,EMBEZZLEMENT,EXTORTION,FAMILY OFFENSES,FORGERY/COUNTERFEITING,FRAUD,GAMBLING,KIDNAPPING,LARCENY/THEFT,LIQUOR LAWS,LOITERING,MISSING PERSON,NON-CRIMINAL,OTHER OFFENSES,PORNOGRAPHY/OBSCENE MAT,PROSTITUTION,RECOVERED VEHICLE,ROBBERY,RUNAWAY,SECONDARY CODES,SEX OFFENSES FORCIBLE,SEX OFFENSES NON FORCIBLE,STOLEN PROPERTY,SUICIDE,SUSPICIOUS OCC,TREA,TRESPASS,VANDALISM,VEHICLE THEFT,WARRANTS,WEAPON LAWS"
+
+  dec = [[(i + 1.0) / 2.0 for i in j] for j in dec]
 
   fmt=['%d'] + ['%1.3f'] * dec.shape[1]
 
